06f5b5388583af99a3c87b1586c84bba65c0e973
nofield <imadeddine799@gmail.com>
2024-06-20 00:16:09 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    double init_seconds;
+    cout << "Enter number of seconds: " << endl;
+    cin >> init_seconds;
+
+    float days = init_seconds / 86400;
+    float hours = (days - (int)days) * 24;
+    float minutes = (hours - (int)hours) * 60;
+    int seconds = (minutes - (int)minutes) * 60;
+
+    cout << init_seconds << " seconds has: " << int(days) << " days, "
+         << int(hours) << " hours, "
+         << int(minutes) << " minutes and "
+         << seconds << " seconds" << endl;
+    return 0;
+}
